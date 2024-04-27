bd4b6262bf5a90e7dbbc38e081c7b043810e1eec
VolhaZha <olgazhauryd@gmail.com>
2024-04-27 18:34:38 +0200

    Task70

---
+++ PageObjectModelsProject_1/Task70_Pages/LogInPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumVZhTestProject_1;
+
+namespace PageObjectModelsProject_1.Task70_Pages
+{
+    public class LogInPage : BasePage
+    {
+        IWebElement _nameField;
+        IWebElement _passField;
+        IWebElement _loginButton;
+
+        public LogInPage(IWebDriver driver) : base(driver)
+        {
+            _nameField = FindElementBy(LogInOutParams.NAME_FIELD);
+            _passField = FindElementBy(LogInOutParams.PASS_FIELD);
+            _loginButton = FindElementBy(LogInOutParams.LOGIN_BUTTON);
+        }
+
+        public void EnterCreds(string name, string password)
+        {
+            _nameField?.SendKeys(name);
+            _passField?.SendKeys(password);
+            _loginButton?.Click();
+        }
+
+        public bool CheckLogoutResult()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            var result = wait.Until(condition =>
+            {
+                try
+                {
+                    var elementToBeDisplayed = _loginButton;
+                    return elementToBeDisplayed.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+            return result;
+        }
+    }
+}
